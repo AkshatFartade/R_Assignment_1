c4288ed91247eab8e65f832c0593256d83bcae0a
AkshatFartade <166796491+AkshatFartade@users.noreply.github.com>
2025-01-31 15:47:31 +0530

    Update and rename Q_18 to Q_18.r

---
+++ Q_18.r
@@ -0,0 +1,3 @@
+# Generate and store a sequence of values from 10 to -20 in steps of 0.5. Reverse this sequence efficiently without recreating it manually.
+seq_values <- seq(10, -20, by = -0.5)
+rev_seq_values <- rev(seq_values)  
